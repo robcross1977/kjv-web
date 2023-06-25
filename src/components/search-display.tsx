4c8f549c1374a108700601a73fe26e6502dad544
Robert Crossland <robertcrossland@crosslandcreative.com>
2023-06-24 22:05:35 -0500

    fixed issue with capitalizing first letter if it is a number

--- src/components/search-display.tsx
+++ src/components/search-display.tsx
@@ -1,7 +1,15 @@
 import { BookRecords } from "kingjames";
 
 function capitalizeFirstLetter(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  if (str.length > 3) {
+    if (new RegExp(/^[a-zA-Z]/).test(str.charAt(0))) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    } else if (new RegExp(/^[a-zA-Z]/).test(str.charAt(2))) {
+      return str.slice(0, 2) + str.charAt(2).toUpperCase() + str.slice(3);
+    }
+  }
+
+  return "No Result Found";
 }
 
 export default function SearchDisplay({ book }: { book?: BookRecords }) {
@@ -13,7 +21,7 @@ export default function SearchDisplay({ book }: { book?: BookRecords }) {
         const finalTitle = capitalizeFirstLetter(title);
         return (
           <div key={title} className="flex flex-col w-full h-full">
-            <h1 className="text-4xl font-semibold mb-5">{`The Book of ${finalTitle}`}</h1>
+            <h1 className="text-4xl font-semibold mb-5 text-center">{`${finalTitle}`}</h1>
             {Object.entries(chapters).map(([chapter, verses]) => {
               return (
                 <div key={`${book}${chapter}`}>
