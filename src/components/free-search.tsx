ce0603d10f333f8e7d357f9a08d9973494ddb7ad
Robert Crossland <robertcrossland@crosslandcreative.com>
2023-06-24 23:05:11 -0500

    fixed up style

--- src/components/free-search.tsx
+++ src/components/free-search.tsx
@@ -13,26 +13,51 @@ export default function FreeSearch() {
 
   return (
     <div className="flex flex-col w-10/12 mx-auto my-5 h-screen">
-      <div className="flex flex-row w-full py-5 bg-yellow-600 rounded-lg my-3 justify-start">
-        <input
-          type="text"
-          id="query"
-          className="mr-3 ml-3 bg-gray-50 w-2/12 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Genesis 1:1"
-          required
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-
-        <button
-          onClick={doSearch}
-          className="text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+      <div className="flex flex-row w-full py-5 bg-yellow-600 rounded-lg my-3 justify-end">
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Search
-        </button>
+        </label>
+        <div className="relative mx-5">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Ex: Genesis 1:2-5"
+            required
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="text-white absolute right-2.5 bottom-2.5 bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={doSearch}
+          >
+            Search
+          </button>
+        </div>
       </div>
 
-      <div className="flex flex-grow bg-parchment rounded-lg py-2.5 px-5 mr-2 mb-2">
+      <div className="flex flex-grow w-full bg-parchment rounded-lg py-2.5 px-5 mr-2 mb-2">
         <SearchDisplay book={results} />
       </div>
     </div>
