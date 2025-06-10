0c77dd0198c3d5330e8b8cdf56f0d327489c2942
Robert Crossland <robertcrossland@gmail.com>
2025-06-10 13:10:59 -0500

    got auth working again

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/avatar/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -2,6 +2,6 @@ import { handleAuth } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   onError(_req: Request, error: Error) {
-    console.error(error);
+    console.error("Auth0 Error:", error);
   },
 });
